b61e712fc2e84e398221ded7efd966827d85c401
chadaaan <123583716+chadaaan@users.noreply.github.com>
2023-01-26 02:19:56 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.*; 
+  
+ public class Main 
+ { 
+         public static void main(String[] args) 
+         { 
+                 Customer C; 
+                 Customer rc = new RegularCustomer("Charles", 1000); 
+             Customer sc = new SeniorCustomer("Mike", 1000); 
+          
+                 C = rc; 
+                 System.out.println(C.getName() + "(Regular Costumer)"); 
+                 System.out.println("Total: $" + C.calculateBill()); 
+                  
+                 C = sc; 
+                 System.out.println(C.getName() + "(Senior Costumer)"); 
+                 System.out.println("Total: $" + C.calculateBill()); 
+                  
+    
+ } 
+          
+  
+ }
