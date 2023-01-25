3e8341d0e619380c49029c0ac76239c1f490f654
chadaaan <123583716+chadaaan@users.noreply.github.com>
2023-01-26 02:22:21 +0800

    Create Regular-Customer.java

---
+++ Regular-Customer.java
@@ -0,0 +1,13 @@
+public class RegularCustomer extends Customer 
+ { 
+          
+         double calculateBill(){ 
+                 return amount; 
+         } 
+          
+         RegularCustomer(String name, double amount){ 
+                 super(name, amount); 
+         } 
+ 
+ }
+ 
