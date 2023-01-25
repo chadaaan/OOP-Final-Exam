b84fad171edd1865bb0c9165a82064f1c8d1f12b
chadaaan <123583716+chadaaan@users.noreply.github.com>
2023-01-26 02:16:59 +0800

    Update Customer-Main.java

--- Customer-Main.java
+++ Customer-Main.java
@@ -1,8 +1,8 @@
 public class Main { 
          public static void main(String[] args) { 
                  Customer customer; 
-                 RegularCustomer regularCustomer = new RegularCustomer("Samanta", 3000); 
-                 SeniorCustomer seniorCustomer = new SeniorCustomer("Edgar", 3000); 
+                 RegularCustomer regularCustomer = new RegularCustomer("Denzel", 3000); 
+                 SeniorCustomer seniorCustomer = new SeniorCustomer("Joseph", 3000); 
   
                  customer = regularCustomer; 
                  System.out.println(customer.getName()+" " +customer.calculateBill()); 
