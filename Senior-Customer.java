84fa0acd7f9d1b5b345d1bf7537cc542d96c03a6
chadaaan <123583716+chadaaan@users.noreply.github.com>
2023-01-26 02:25:10 +0800

    Create Senior-Customer.java

---
+++ Senior-Customer.java
@@ -0,0 +1,15 @@
+public class SeniorCustomer extends Customer 
+ { 
+         final static double discount = 0.20; 
+          
+         double calculateBill(){ 
+                 amount = amount - (amount * discount); 
+                 return amount; 
+         } 
+          
+         SeniorCustomer(String name, int amount){ 
+                 super(name, amount); 
+         } 
+    
+ }
+ 
